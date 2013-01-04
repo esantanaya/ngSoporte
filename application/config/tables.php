685e07a30e745f98d88febccc8494f34c695a330
esantanaya <esantanaya@gmail.com>
2013-01-03 20:12:31 -0600

    Se configura la encripcion

---
+++ application/config/tables.php
@@ -0,0 +1,21 @@
+<?php
+/*
+*	Tablas de la base de datos
+*/
+
+$config['tablas']['sop_syslog'] = 'syslog';
+$config['tablas']['tk_datos_adjuntos'] = 'datos';
+$config['tablas']['tk_lock'] = 'lock';
+$config['tablas']['tk_mensaje'] = 'mensaje';
+$config['tablas']['tk_notas'] = 'notas';
+$config['tablas']['tk_prioridad'] = 'prioridad';
+$config['tablas']['tk_respuesta'] = 'respuesta';
+$config['tablas']['tk_ticket'] = 'ticket';
+$config['tablas']['us_departamentos'] = 'departamentos';
+$config['tablas']['us_email'] = 'email';
+$config['tablas']['us_grupos'] = 'grupos';
+$config['tablas']['us_tipos_usuarios'] = 'tipos';
+$config['tablas']['us_usuarios'] = 'usuarios';
+
+/* End of file tables.php */
+/* Location: ./application/config/tables.php */
--- application/models/usuario_model.php
+++ application/models/usuario_model.php
@@ -2,10 +2,16 @@
 
 class Usuario_model extends CI_Model {
 
+	var $tablas = array();
+
 	public function __construct()
 	{
 		parent::__construct();
 		//Do your magic here
+	}
+
+	function insert_usuario() {
+		$this
 	}
 }
 
