f49b2d84e82ac32c202de09bf5e7dab71b71e760
esantanaya <esantanaya@gmail.com>
2013-02-08 19:17:20 -0600

    Se agrega la funcion date_diff a la custom library para los servidores con php < 5.3.0

--- application/helpers/customlibrary_helper.php
+++ application/helpers/customlibrary_helper.php
@@ -228,6 +228,20 @@ if (!function_exists('arma_menu')) {
 	}
 
 }
+
+if (!function_exists('date_diff'))
+{
+	function date_diff($date1, $date2) { 
+    $current = $date1; 
+    $datetime2 = date_create($date2); 
+    $count = 0; 
+    while(date_create($current) < $datetime2){ 
+        $current = gmdate("Y-m-d", strtotime("+1 day", strtotime($current))); 
+        $count++; 
+    } 
+    return $count; 
+}
+}
 /* * **************************************
  * 	FUNCIONES DE FORMATO				*
  * ************************************** */
