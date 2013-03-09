a1d6400feb6a1513746af368749fc8bdafbdc86a
esantanaya <esantanaya@gmail.com>
2013-03-09 11:35:25 -0600

    Se comienza la parte de cambos a mi cuenta Se termina edicion de usuarios

--- application/controllers/admin/usuarios.php
+++ application/controllers/admin/usuarios.php
@@ -18,11 +18,31 @@ class Usuarios extends CI_Controller {
 			redirect(base_url() . 'cambia_pass');
 
 		$this->load->helper('date');
+		$this->load->library('table');
+		error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
 	}
 
 	public function index($value='')
 	{
-		# code...
+		$this->lista();
+	}
+
+	public function lista()
+	{
+
+		$listado = $this->usuario_model->get_usuarios_listado();
+		$tmpl = array('table_open' => '<table border="0" cellpadding="4"
+				cellspacing="0" class="listado_table">');
+		$this->table->set_template($tmpl);
+
+		$data['SYS_MetaTitle'] = 'Tickets :: Usuarios';
+		$data['SYS_metaKeyWords'] = 'sistema ticket n&g';
+		$data['SYS_metaDescription'] = 'Listado de Usuario del sistema';
+		$data['subMenu'] = 'admin/submenu_view';
+		$data['modulo'] = 'admin/lista_usuario_view';
+		$data['listado'] = $listado;
+
+		$this->load->view('admin/main_admin_view', $data);
 	}
 
 	public function nuevo($cod_usuario = null, $clave = null, $id = null)
@@ -62,6 +82,83 @@ class Usuarios extends CI_Controller {
 			$data['id_usuario'] = $id;
 
 		$this->load->view('admin/main_admin_view', $data);
+	}
+
+	public function edita($cod_usuario)
+	{
+		$generales = $this->usuario_model->get_edita_usuario($cod_usuario);
+		$nivel = $generales->id_nivel_usuario;
+
+		if ($nivel <= 2)
+		{
+			$this->edita_usuario($cod_usuario);
+		}
+		else
+		{
+			$this->edita_cliente($cod_usuario);	
+		}
+
+	}
+
+	public function edita_usuario($cod_usuario, $clave = null, $id = null)
+	{
+		$generales = $this->usuario_model->get_edita_usuario($cod_usuario);
+		$datos = $this->usuario_model->get_departamentos_id();
+
+		foreach ($datos as $depas => $valor) 
+		{
+			$select[$valor['dept_id']] = $valor['dept_name'];
+		}
+
+		$datos = $this->usuario_model->get_roles_staff();
+
+		foreach ($datos as $key => $value) 
+		{
+			$niveles[$value['idRol']] = $value['nombreRol'];
+		}
+
+		$data['SYS_MetaTitle'] = 'Tickets :: Usuarios';
+		$data['SYS_metaKeyWords'] = 'sistema ticket n&g';
+		$data['SYS_metaDescription'] = 'Panel de edición de usuarios';
+		$data['subMenu'] = 'admin/submenu_view';
+		$data['modulo'] = 'admin/edita_usuario_view';
+		$data['depts'] = $select;
+		$data['niveles'] = $niveles;
+		$data['deptActual'] = $generales->id_departamento_usuario;
+		$data['nivelActual'] = $generales->id_nivel_usuario;
+		$data['nombre'] = $generales->nombre_usuario;
+		$data['apPaterno'] = $generales->apellido_paterno;
+		$data['apMaterno'] = $generales->apellido_materno;
+		$data['correo'] = $generales->email_usuario;
+		$data['nombre'] = $generales->nombre_usuario;
+		$data['tel'] = $generales->tel_usuario;
+		$data['ext'] = $generales->ext_usuario;
+		$data['cel'] = $generales->movil_usuario;
+		$data['firma'] = $generales->firma_usuario;
+		$data['cambioPass'] = ($generales->cambia_pass == 1) ? 'checked' : '' ;
+		$data['activoCheck'] = ($generales->activo == 1) ? 'checked': '';
+		$data['bloqueadoCheck'] = ($generales->activo == 0) ? 'checked': '';
+		$data['listadoCheck'] = ($generales->visible == 1) ? 'checked' : '';
+		$data['vacacionCheck'] = ($generales->vacacion == 1) ? 'checked' : '';
+		$data['cod_usuario'] = '';
+		$data['clave'] = '';
+		$data['id_usuario'] = '';
+
+		if ($cod_usuario != null)
+			$data['cod_usuario'] = $cod_usuario;
+		
+		if ($clave != null)
+			$data['clave'] = $clave;
+
+		if ($id != null)
+			$data['id_usuario'] = $id;
+
+		$this->load->view('admin/main_admin_view', $data);
+	}
+
+	public function edita_cliente($cod_usuario)
+	{
+		# code...
 	}
 
 	public function cliente($cod_usuario = null, $clave = null, $id = null)
@@ -296,6 +393,52 @@ class Usuarios extends CI_Controller {
 			
 			$this->nuevo(null, null, $id_usuario);
 		}
+	}
+
+	public function guarda_cambios()
+	{
+		$cod_usuario = $this->input->post('cod_usuario');
+		$id_usuario = $this->usuario_model->get_id_usuario($cod_usuario);
+		$pass_usuario = $this->input->post('pass_usuario');
+		$confirma_pass = $this->input->post('confirma_pass');
+		$cambia_pass = ($this->input->post('cambia_pass') == 'on') ? 1 : 0;
+		$activo = ($this->input->post('activo') == 'activo') ? 1 : 0;
+		$visible = ($this->input->post('listado') == 'on') ? 1 : 0;
+		$vacacion = ($this->input->post('vacacion') == 'on') ? 1 : 0;
+
+		if ($pass_usuario != $confirma_pass)
+		{
+			$badPass = 'Las Contrase&ntilde;as no coinciden';
+			$this->edita_usuario($cod_usuario, $badPass);
+			return false;
+		}
+		
+		$data = array('id_departamento_usuario' => $this->input->post(
+											   'departamento'), 
+				  'id_nivel_usuario' => $this->input->post('nivel'),
+				  'nombre_usuario' => $this->input->post('nombre_usuario'),
+				  'apellido_paterno' => $this->input->post(
+				  						 'apellido_paterno'),
+				   'apellido_materno' => $this->input->post(
+				   						 'apellido_materno'),
+				   'email_usuario' => $this->input->post('mail_usuario'),
+				   'tel_usuario' => $this->input->post('tel_usuario'),
+				   'ext_usuario' => $this->input->post('ext_tel'),
+				   'movil_usuario' => $this->input->post('cel_usuario'),
+				   'firma_usuario' => $this->input->post('firma_usuario'),
+				   'cambia_pass' => $cambia_pass,
+				   'activo' => $activo,
+				   'visible' => $visible,
+				   'vacacion' => $vacacion);
+
+		if (! empty($pass_usuario))
+		{
+			$this->usuario_model->update_password_usuario($id_usuario, 
+														  $pass_usuario);
+		}
+		
+		$this->usuario_model->update_usuario_cuenta($cod_usuario, $data);
+		$this->edita_usuario($cod_usuario);
 	}
 
 }
